815617504296d7d95621d345580dc70806242f00
skortmann <skortmann@ethz.ch>
2022-04-13 22:30:31 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysing the influence of the market clearing algorithm EUPHEMIA on the integration process of market coupling.',
+    author='Steffen Kortmann',
+    license='MIT',
+)
